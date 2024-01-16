29136fe71af9bb34c8c7210f3a02f2c18b3e4134
Fernando Vilgino Sbampato <fernando@asti2012.com.br>
2024-01-16 15:07:16 -0300

    first commit

---
+++ app/src/main/java/Activity/MainActivity.java
@@ -0,0 +1,18 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.cogna.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
