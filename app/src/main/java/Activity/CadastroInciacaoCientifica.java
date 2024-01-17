cf534d093f91e019298f000d9695bb638897746c
Fernando Vilgino Sbampato <fernando@asti2012.com.br>
2024-01-17 12:07:11 -0300

    third commit

---
+++ app/src/main/java/Activity/CadastroInciacaoCientifica.java
@@ -0,0 +1,20 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import com.example.cogna.R;
+
+public class CadastroInciacaoCientifica extends AppCompatActivity {
+
+    Spinner activity_cadastro_iniciacao_cientifica_editText_sexo_recebido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_inciacao_cientifica);
+
+    }
+}
