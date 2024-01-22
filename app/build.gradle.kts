2fa879fe6478b7e522f70f32112b0762a3a215d2
Fernando Vilgino Sbampato <fernando@asti2012.com.br>
2024-01-22 13:40:52 -0300

    six commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    //autenticação
+    implementation("com.google.firebase:firebase-auth:18.0.0")
 }
