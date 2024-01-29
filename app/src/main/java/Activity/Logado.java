86fd0093e0975a909219e89e6f46724272b308d4
Fernando Vilgino Sbampato <fernando@asti2012.com.br>
2024-01-29 12:13:38 -0300

    twelve commit

--- app/src/main/java/Activity/Logado.java
+++ app/src/main/java/Activity/Logado.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.example.cogna.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,8 +21,35 @@ public class Logado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logado);
 
+        //autenticação no firebase
         auth = ConfiguracaoBD.firebaseautenticacao();
+
+        //validação do campo checkbox
+        CheckBox checkBox = findViewById(R.id.como_previsto);
+        CheckBox checkBox1 = findViewById(R.id.adiantado);
+        CheckBox checkBox2 = findViewById(R.id.atrasado);
+        CheckBox checkBox3 = findViewById(R.id.nao_iniciado);
+        CheckBox checkBox4 = findViewById(R.id.concluido);
     }
+
+    public void onCheckboxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+
+        int itemID = view.getId();
+
+        if(itemID == R.id.como_previsto) {
+            Toast.makeText(this, "Como previsto", Toast.LENGTH_SHORT).show();
+        } else if (itemID == R.id.adiantado ) {
+            Toast.makeText(this, "Adiantado", Toast.LENGTH_SHORT).show();
+        } else if (itemID == R.id.atrasado) {
+            Toast.makeText(this, "Atrasado", Toast.LENGTH_SHORT).show();
+        } else if (itemID == R.id.nao_iniciado) {
+            Toast.makeText(this, "Não iniciado", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Concluido", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     public void deslogar(View view){
         try {
