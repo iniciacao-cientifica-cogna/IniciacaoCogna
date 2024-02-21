9ff9aeed502cfaabc1f22f0de00aa8de68050ff0
ledrodh <ledrodh@protonmail.com>
2024-02-21 14:45:11 -0300

    Start commite

--- app/src/main/java/Activity/MeioAmbiente.java
+++ app/src/main/java/Activity/MeioAmbiente.java
@@ -9,11 +9,31 @@ import com.example.cogna.R;
 
 public class MeioAmbiente extends AppCompatActivity {
 
-    Spinner activity_meio_ambiente_spinner;
+    // criação dos spinners
+    Spinner activity_meio_ambiente_levantamentoBibliografico_spinner,
+            activity_meio_ambiente_STF_spinner,
+            activity_meio_ambiente_revisaoBibliografica_spinner,
+            activity_meio_ambiente_elaboracaoEscrita_spinner,
+            activity_meio_ambiente_analiseDiscurso_spinner,
+            activity_meio_ambiente_revisãoDados_spinner,
+            activity_meio_ambiente_elaboracaoTextoFinal_spinner;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meio_ambiente);
-        activity_meio_ambiente_spinner = findViewById(R.id.activity_meio_ambiente_spinner);
+       // recebimento dos valores selecionados pelos alunos //
+
+        activity_meio_ambiente_levantamentoBibliografico_spinner = findViewById(R.id.activity_meio_ambiente_levantamentoBibliografico_spinner);
+        activity_meio_ambiente_STF_spinner = findViewById(R.id.activity_meio_ambiente_STF_spinner);
+        activity_meio_ambiente_revisaoBibliografica_spinner = findViewById(R.id.activity_meio_ambiente_revisaoBibliografica_spinner);
+        activity_meio_ambiente_elaboracaoEscrita_spinner = findViewById(R.id.activity_meio_ambiente_elaboracaoEscrita_spinner);
+        activity_meio_ambiente_analiseDiscurso_spinner = findViewById(R.id.activity_meio_ambiente_analiseDiscurso_spinner);
+        activity_meio_ambiente_revisãoDados_spinner= findViewById(R.id.activity_meio_ambiente_revisãoDados_spinner);
+        activity_meio_ambiente_elaboracaoTextoFinal_spinner= findViewById(R.id.activity_meio_ambiente_elaboracaoTextoFinal_spinner);
     }
 }
