2ba91e0e5c3c9b34dd4d2c063edc2ce883a61146
Fernando Vilgino Sbampato <fernando@asti2012.com.br>
2024-02-22 12:53:05 -0300

    Twenty nine commit

--- app/src/main/java/Activity/Odontologia.java
+++ app/src/main/java/Activity/Odontologia.java
@@ -3,17 +3,30 @@ package Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 
 import com.example.cogna.R;
 
 public class Odontologia extends AppCompatActivity {
 
-    Spinner
+    Spinner pergunta_treinamento_odontologia,
+            pergunta_recrutamento_odontologia,
+            pergunta_coleta_odontologia,
+            pergunta_analise_odontologia,
+            pergunta_divulgacao_odontologia;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_odontologia);
-
+    }
+    public void validar_campos(View view){
+        pergunta_treinamento_odontologia = findViewById(R.id.pergunta_treinamento_odontologia);
+        pergunta_recrutamento_odontologia= findViewById(R.id.pergunta_recrutamento_odontologia);
+        pergunta_coleta_odontologia = findViewById(R.id.pergunta_coleta_odontologia);
+        pergunta_analise_odontologia = findViewById(R.id.pergunta_analise_odontologia);
+        pergunta_divulgacao_odontologia = findViewById(R.id.pergunta_divulgacao_odontologia);
     }
 }
--- app/src/main/java/Activity/TIC.java
+++ app/src/main/java/Activity/TIC.java
@@ -3,17 +3,46 @@ package Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Spinner;
 
 import com.example.cogna.R;
 
 public class TIC extends AppCompatActivity {
 
-    Spinner
+    Spinner pergunta_reuniao_tic,
+            pergunta_producao_tic,
+            pergunta_elaboracao_tic,
+            pergunta_apoio_tic,
+            pergunta_criacao_tic,
+            pergunta_gestao_tic,
+            pergunta_edicao_tic,
+            pergunta_ajustes_tic,
+            pergunta_relatorio_tic,
+            pergunta_coordenador_tic,
+            pergunta_reunioes_tic,
+            pergunta_avaliacao_tic,
+            pergunta_final_tic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic);
 
+    }
+
+    public void validar_campos(View view){
+        pergunta_reuniao_tic = findViewById(R.id.pergunta_reuniao_tic);
+                pergunta_producao_tic = findViewById(R.id.pergunta_producao_tic);
+                pergunta_elaboracao_tic = findViewById(R.id.pergunta_elaboracao_tic);
+                pergunta_apoio_tic = findViewById(R.id.pergunta_apoio_tic);
+                pergunta_criacao_tic = findViewById(R.id.pergunta_criacao_tic);
+                pergunta_gestao_tic = findViewById(R.id.pergunta_gestao_tic);
+                pergunta_edicao_tic = findViewById(R.id.pergunta_edicao_tic);
+                pergunta_ajustes_tic = findViewById(R.id.pergunta_ajustes_tic);
+                pergunta_relatorio_tic = findViewById(R.id.pergunta_relatorio_tic);
+                pergunta_coordenador_tic = findViewById(R.id.pergunta_coordenador_tic);
+                pergunta_reunioes_tic = findViewById(R.id.pergunta_reunioes_tic);
+                pergunta_avaliacao_tic = findViewById(R.id.pergunta_avaliacao_tic);
+                pergunta_final_tic = findViewById(R.id.pergunta_final_tic);
     }
 }
